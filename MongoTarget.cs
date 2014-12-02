9fae45f7f64f64a89496e4c086c580d901cb6021
Dmitry <stormbringer-s@mail.ru>
2014-12-02 13:02:14 +0400

    Update MongoTarget.cs

--- MongoTarget.cs
+++ MongoTarget.cs
@@ -1,55 +1,65 @@
-    class MongoTarget : NLog.Targets.Target
+using System;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using NLog.Common;
+using NLog.Config;
+using NLog.Targets;
+
+namespace NLog.MongoDB
+{
+    [Target("MongoDB")]
+    public class MongoTarget : Target
     {
-        private readonly string _mongoCS;
-        private readonly string _db;
-        private readonly string _collection;
+
         private MongoCollection<BsonDocument> _coll;
         public byte ExceptionRecursionLevel { get; set; }
         public bool Capped { get; set; }
         public int CappedSizeMB { get; set; }
+
+
+        public string AppName { get; set; }
+        [RequiredParameter]
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        private string CollectionName { get; set; }
         public MongoTarget()
         {
             Capped = true;
             CappedSizeMB = 200;
             ExceptionRecursionLevel = 2;
-            _collection = App.Current.GetType().Namespace + "Log";
-            _db = "Logs";
+            DatabaseName = "Logs";
+            AppName = "Application";
         }
-        public MongoTarget(string mongoCS)
-            : this()
-        {
-            _mongoCS = mongoCS;
-            _coll = GetCollection();
-        }
+
         long MB(long count)
         {
             return (long)(count * Math.Pow(1024, 2));
         }
-        MongoCollection<BsonDocument> GetCollection()
+
+        protected override void InitializeTarget()
         {
-            var db = new MongoClient(_mongoCS).GetServer().GetDatabase(_db);
-            if (!db.CollectionExists(_collection))
+            if (_coll == null)
             {
-                var b = new MongoDB.Driver.Builders.CollectionOptionsBuilder();
-                if (Capped)
+                CollectionName = AppName + "Log";
+                var db = new MongoClient(ConnectionString).GetServer().GetDatabase(DatabaseName);
+                if (!db.CollectionExists(CollectionName))
                 {
-                    b = b.SetCapped(true).SetMaxSize(MB(CappedSizeMB));
+                    var b = new CollectionOptionsBuilder();
+                    if (Capped)
+                    {
+                        b = b.SetCapped(true).SetMaxSize(MB(CappedSizeMB));
+                    }
+                    db.CreateCollection(CollectionName, b);
                 }
-                db.CreateCollection(_collection, b);
+                _coll = db.GetCollection(CollectionName);
             }
-            return db.GetCollection(_collection);
-        }
-        public MongoTarget(string mongoCS, string db, string collection)
-            : this()
-        {
-            _mongoCS = mongoCS;
-            _db = db;
-            _collection = collection;
-            _coll = GetCollection();
         }
         protected override void Write(AsyncLogEventInfo[] logEvents)
         {
             var docs = logEvents.Select(l => GetDocFromLogEventInfo(l.LogEvent));
+
             _coll.InsertBatch(docs);
         }
         protected override void Write(LogEventInfo logEvent)
@@ -66,26 +76,42 @@
             doc.Add("LocalDate", DateTime.Now);
             doc.Add("UTCDate", DateTime.Now.ToUniversalTime());
             doc.Add("Message", logEvent.FormattedMessage);
+            doc.Add("AppName", AppName);
             if (logEvent.Exception != null)
             {
                 doc.Add("Exception", GetException(logEvent.Exception));
             }
             return doc;
         }
-        BsonDocument GetException(Exception ex)
+        BsonDocument GetException<T>(T ex) where T : Exception
         {
             return GetException(ex, ExceptionRecursionLevel);
         }
-        BsonDocument GetException(Exception ex, byte level)
+        BsonDocument GetException<T>(T ex, byte level) where T : Exception
         {
             var doc = new BsonDocument();
-            doc.Add("Message", ex.Message);
-            doc.Add("StackTrace", ex.StackTrace);
-            doc.Add("Source", ex.Source);
+            AddStringProperties(ex, doc);
+            doc.Add("TargetSite", ex.TargetSite.Name);
+            if (ex.TargetSite.DeclaringType != null)
+            {
+                doc.Add("ClassName", ex.TargetSite.DeclaringType.FullName);
+            }
             if (ex.InnerException != null && level > 0)
             {
                 doc.Add("InnerException", GetException(ex.InnerException, (byte)(level - 1)));
             }
             return doc;
         }
+
+        private void AddStringProperties(Exception exception, BsonDocument doc)
+        {
+            var props = exception.GetType().GetProperties().Where(x => x.PropertyType == typeof(string));
+            foreach (var pi in props)
+            {
+                string s = (string)pi.GetValue(exception);
+                doc.Add(pi.Name, s);
+            }
+        }
     }
+
+}
